6821e2cbd99734ac5bb01fb6661a29b21b0b4a17
AlinaF <afassahova9@gmail.com>
2022-09-05 21:13:33 +0500

    add reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int price = cost;
+        int milli = 20;
+        int bonus = price / milli;
+        System.out.println(price / milli);
+        return bonus;
+    }
 }
